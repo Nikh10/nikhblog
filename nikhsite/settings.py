3b9b26e255d0b0615c47c2d2a74c07bbeb11ed39
nikhilkumar <nikhil.k@3ktechnologies.com>
2019-04-20 23:09:18 +0530

    updated requirements file

--- nikhsite/settings.py
+++ nikhsite/settings.py
@@ -113,7 +113,4 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+
